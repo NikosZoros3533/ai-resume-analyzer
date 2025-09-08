6275736d069a7dea3e73b54972078739b592143e
NikosZoros3533 <nikozoro@csd.auth.gr>
2025-09-08 12:12:58 +0300

    form created for the job and resume input

---
+++ app/lib/utils.ts
@@ -0,0 +1,16 @@
+/**
+ * Formats a file in bytes to a human-readable string (KB, MB, GB)
+ * @param bytes
+ * @returns A formatted string with the appropriate unit
+ */
+
+export function formatSize(bytes: number): string {
+  if (bytes === 0) return "0 Bytes";
+
+  const k = 1024;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+
+  //Determine the appropriate unit by calculating the log
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
+}
